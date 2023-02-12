c760cbf222b26080d5f8ac7e6caf49bf11f7736d
Stanislav Dumchykov <mechanikea@gmail.com>
2023-02-12 02:00:16 +0200

    Implemented TabLayout. There are issues with popping up SignUpActivity → MainActivity backstack, updating count of contacts in TabLayout.

--- app/src/main/java/sdumchykov/androidApp/presentation/MainActivity.kt
+++ app/src/main/java/sdumchykov/androidApp/presentation/MainActivity.kt
@@ -2,14 +2,29 @@ package sdumchykov.androidApp.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
 import sdumchykov.androidApp.R
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.mainActivityNavHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+//        setupActionBarWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
