fc34cd9fdc99bff8aa7070bf1c8699fea3af4b8d
Stanislav Dumchykov <mechanikea@gmail.com>
2024-09-03 09:42:34 +0300

    Completed EditProfileFragment.

--- app/src/main/java/com/dumchykov/socialnetworkdemo/ui/editprofile/EditProfileFragment.kt
+++ app/src/main/java/com/dumchykov/socialnetworkdemo/ui/editprofile/EditProfileFragment.kt
@@ -5,14 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.dumchykov.socialnetworkdemo.data.webapi.ResponseState
 import com.dumchykov.socialnetworkdemo.databinding.FragmentEditProfileBinding
+import com.dumchykov.socialnetworkdemo.domain.webapi.models.EditUserResponse
+import com.dumchykov.socialnetworkdemo.ui.SharedViewModel
+import com.dumchykov.socialnetworkdemo.ui.util.handleStandardResponse
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class EditProfileFragment : Fragment() {
     private var _binding: FragmentEditProfileBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: EditProfileViewModel by viewModels()
+    private val sharedViewModel: SharedViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
@@ -23,7 +33,10 @@ class EditProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        fillInDataInInputFields()
+        setArrowBackClickListener()
         setSaveClickListener()
+        observeEditUserAttempt()
     }
 
     override fun onDestroyView() {
@@ -31,10 +44,57 @@ class EditProfileFragment : Fragment() {
         _binding = null
     }
 
+    private fun observeEditUserAttempt() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.editProfileState.collect { state ->
+                handleStandardResponse(
+                    state = state,
+                    context = requireContext(),
+                    scope = this,
+                    progressLayout = binding.layoutProgress.root
+                ) {
+                    binding.layoutProgress.root.visibility = View.GONE
+                    val (user) = (state as ResponseState.Success<*>).data as EditUserResponse
+                    sharedViewModel.updateState {
+                        copy(
+                            currentUser = user
+                        )
+                    }
+                    findNavController().navigateUp()
+                }
+            }
+        }
+    }
+
     private fun setSaveClickListener() {
         binding.buttonSave.setOnClickListener {
-            // TODO: call update user data api
+            val userId = sharedViewModel.shareState.value.currentUser.id
+            val bearerToken = sharedViewModel.shareState.value.accessToken
+            val contact = sharedViewModel.shareState.value.currentUser.copy(
+                name = binding.textInputUserNameEditText.text.toString(),
+                career = binding.textInputCareerEditText.text.toString(),
+                email = binding.textInputEmailEditText.text.toString(),
+                phone = binding.textInputPhoneEditText.text.toString(),
+                address = binding.textInputAddressEditText.text.toString(),
+                birthday = binding.textInputBirthDateEditText.text.toString()
+            )
+            viewModel.editProfile(userId, bearerToken, contact)
+        }
+    }
+
+    private fun setArrowBackClickListener() {
+        binding.buttonArrowBack.setOnClickListener {
             findNavController().navigateUp()
         }
+    }
+
+    private fun fillInDataInInputFields() {
+        val currentUser = sharedViewModel.shareState.value.currentUser
+        binding.textInputUserNameEditText.setText(currentUser.name)
+        binding.textInputCareerEditText.setText(currentUser.career)
+        binding.textInputEmailEditText.setText(currentUser.email)
+        binding.textInputPhoneEditText.setText(currentUser.phone)
+        binding.textInputAddressEditText.setText(currentUser.address)
+        binding.textInputBirthDateEditText.setText(currentUser.birthday)
     }
 }
